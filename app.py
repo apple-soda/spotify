0a6839ee6175dec12e1b8658a70245f581f0f1b2
apple-soda <kekpyds@gmail.com>
2022-11-15 17:51:57 -0800

    kek

---
+++ app.py
@@ -0,0 +1,56 @@
+from flask import Flask, render_template, request, redirect, url_for
+import random
+import copy
+
+from utils import *
+
+## SECRET SHIT (CHANGE FOR GITHUB PUSHES)
+# id and shit
+CLIENT_ID = '9bc3dea17d8e4cb98694606b0bd7c618'
+CLIENT_SECRET = 'a8c34b47f88940e498031d554aa96430'
+
+AUTH_URL = 'https://accounts.spotify.com/api/token'
+
+# POST
+auth_response = requests.post(AUTH_URL, {
+    'grant_type': 'client_credentials',
+    'client_id': CLIENT_ID,
+    'client_secret': CLIENT_SECRET,
+})
+
+# convert the response to JSON
+auth_response_data = auth_response.json()
+BASE_URL = 'https://api.spotify.com/v1/'
+# save the access token
+access_token = auth_response_data['access_token']
+
+headers = {
+    'Authorization': 'Bearer {token}'.format(token=access_token)
+}
+
+app = Flask(__name__)
+
+@app.route('/', methods=['POST', 'GET'])
+def main():
+    if request.method == 'POST':
+        link = request.form['link']
+        
+        # do some transformations
+        track_id = link.split('/')[-1].split('?')[0]
+        playlist_id = link.split('/')[-1]
+
+        raw_playlist_data = get_raw_playlist_data(BASE_URL, playlist_id, headers)
+
+        playlist_list = get_entire_playlist(BASE_URL, track_id, headers)
+        artists_d = get_artist_count(playlist_list)
+
+        # lot of parameters to pass lowkey
+        # NOTE: 
+        return render_template(
+            'analytics.html',
+            raw_playlist_data=raw_playlist_data,
+            artists_d=artists_d
+        )
+
+    else:
+        return render_template('index.html')
---
+++ utils.py
@@ -0,0 +1,61 @@
+import requests
+import json
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+def get_raw_playlist_data(base_url, playlist_id, headers):
+    r = requests.get(
+        base_url + 'playlists/' + playlist_id + '/tracks',
+        headers=headers,
+        params={'offset': 0}
+    )
+    d = r.json()
+    return d
+
+
+def get_entire_playlist(base_url, track_id, headers):
+    master_list = []
+    offset = 0
+
+    r = requests.get(
+        base_url + 'playlists/' + track_id + '/tracks',
+        headers=headers,
+        params={'offset': offset}
+    )
+
+    d = r.json()
+    master_list += d['items']
+
+    while len(d['items']) > 0:
+        offset += 100
+        r = requests.get(
+            base_url + 'playlists/' + track_id + '/tracks',
+            headers=headers,
+            params={'offset': offset}
+        )
+        d = r.json()
+        master_list += d['items']
+    
+    return master_list
+
+def get_artist_count(master_list):
+    # ['track']['artists']: list of artists
+    # ['track']['artists'][0]['name'] : name of first artist
+    artists = {}
+    
+    for i in master_list:
+        an = i['track']['artists'][0]['name']
+        if an in artists:
+            artists[an] += 1
+        else:
+            artists[an] = 1
+
+    return dict(sorted(artists.items(), key=lambda item: item[1]))
+
+
+def get_album_count(master_list):
+    # ['track']['album']['name'] : album name
+    # ['track']['album']['images'][0] : album image
+    return None
