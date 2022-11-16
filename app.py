bcef75bb356deb5237bde48666d75ec9a654a680
apple-soda <kekpyds@gmail.com>
2022-11-15 18:08:39 -0800

    reformat

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ from utils import *
 # id and shit
 CLIENT_ID = '9bc3dea17d8e4cb98694606b0bd7c618'
 CLIENT_SECRET = 'a8c34b47f88940e498031d554aa96430'
-
 AUTH_URL = 'https://accounts.spotify.com/api/token'
+BASE_URL = 'https://api.spotify.com/v1/'
 
 # POST
 auth_response = requests.post(AUTH_URL, {
@@ -18,15 +18,14 @@ auth_response = requests.post(AUTH_URL, {
     'client_secret': CLIENT_SECRET,
 })
 
-# convert the response to JSON
 auth_response_data = auth_response.json()
-BASE_URL = 'https://api.spotify.com/v1/'
-# save the access token
 access_token = auth_response_data['access_token']
+
 
 headers = {
     'Authorization': 'Bearer {token}'.format(token=access_token)
 }
+
 
 app = Flask(__name__)
 
@@ -40,14 +39,17 @@ def main():
         playlist_id = link.split('/')[-1]
 
         raw_playlist_data = get_raw_playlist_data(BASE_URL, playlist_id, headers)
-
         playlist_list = get_entire_playlist(BASE_URL, track_id, headers)
         artists_d = get_artist_count(playlist_list)
 
+        playlist_img_url = raw_playlist_data['images'][0]['url']
+
+
         # lot of parameters to pass lowkey
-        # NOTE: 
+        # NOTE: urls need to be passed as a separate parameter :/
         return render_template(
             'analytics.html',
+            playlist_img_url=playlist_img_url,
             raw_playlist_data=raw_playlist_data,
             artists_d=artists_d
         )
